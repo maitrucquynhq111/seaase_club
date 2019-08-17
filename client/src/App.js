ed890a7615a98db586c292dbfb93c526f5674c16
Quinn <maitrucquynh111@gmail.com>
2019-08-17 14:16:45 +0700

    up

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      greeting: ''
+    }
+  }
+
+  componentDidMount(){
+    console.log('componentDidMount');
+    axios.get('http://localhost:5000/api/helloworld')
+    .then(result => this.setState({greeting: result.data.sayHi}))
+    .catch(err => console.log(err))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>{this.state.greeting}</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
