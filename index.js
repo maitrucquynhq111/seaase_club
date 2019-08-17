47c8684f7b654e09823524754a30598cc0aca316
Quinn <maitrucquynh111@gmail.com>
2019-08-17 12:03:26 +0700

    update

--- index.js
+++ index.js
@@ -43,6 +43,15 @@ app.use(function (req, res, next) {
     next();
 });
 app.use(cors())
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));  //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
+}
+//build mode
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
 app.get('/', (req, res) => {
    res.send('hello from server!')
 })
