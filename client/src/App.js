241fc851074b5f3789f1eec871a78c6a573ccf40
maitrucquynh111 <maitrucquynh111@gmail.com>
2019-08-18 11:27:24 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -28,24 +28,24 @@ class App extends Component{
 
   render(){
     return (
-      // <Subject/>
-      <div className="App">
-        <header className="App-header">
-          <h1>{this.state.greeting}</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Subject/>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <h1>{this.state.greeting}</h1>
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
     );
   }
 }
