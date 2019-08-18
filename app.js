4b7006b7f4c4d2706e0b69deb8904fdab543bbc6
maitrucquynh111 <maitrucquynh111@gmail.com>
2019-08-18 09:22:42 +0700

    update

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));  //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
 }
 //build mode
-app.get('/', (req, res) => {  res.sendFile(path.join(__dirname+'/client/build/index.html'));})
+app.get('/client', (req, res) => {  res.sendFile(path.join(__dirname+'/client/build/index.html'));})
 
 app.use('/api', RouterControl);
 // app.get('/api/helloworld', (req, res) => {
