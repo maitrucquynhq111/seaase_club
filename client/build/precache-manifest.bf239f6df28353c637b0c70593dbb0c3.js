a24db346712f9c0234882a115fbefea5ed693280
maitrucquynh111 <maitrucquynh111@gmail.com>
2019-08-18 11:58:11 +0700

    up LTE

---
+++ client/build/precache-manifest.bf239f6df28353c637b0c70593dbb0c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7088d78716344718be5dc5ba41aa6b1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d51d864c075e8514951",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e79126bd1ee35bc3a6ad",
+    "url": "/static/js/2.64468b35.chunk.js"
+  },
+  {
+    "revision": "6d51d864c075e8514951",
+    "url": "/static/js/main.13ce5d16.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
